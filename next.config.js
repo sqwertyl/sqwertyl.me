051d62d9849974c321ed03e8101f63a9d9e9ea59
sqwertyl <andrewchen118@gmail.com>
2023-03-20 16:57:49 -0700

    fixed config (:

--- next.config.js
+++ next.config.js
@@ -5,18 +5,17 @@ const nextConfig = {
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
+let assetPrefix = '';
+let basePath = '';
+
 if (isGithubActions) {
   const repo = 'sqwertyl.me'
   repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  let assetPrefix = `/${repo}`
-  let basePath = `/${repo}`
-  module.exports = {
-    assetPrefix: assetPrefix,
-    basePath: basePath,
-  }
+  assetPrefix = `/${repo}`
+  basePath = `/${repo}`
 }
 
 module.exports = {
-  assetPrefix: '',
-  basePath: '',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
 };
