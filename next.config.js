1bed2d8b39eae52a7d49aeb3421c2bb9ac4d4508
sqwertyl <andrewchen118@gmail.com>
2023-03-20 16:59:41 -0700

    fixed config (: pt.2

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ let basePath = '';
 
 if (isGithubActions) {
   const repo = 'sqwertyl.me'
-  repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  // repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
   assetPrefix = `/${repo}`
   basePath = `/${repo}`
 }
