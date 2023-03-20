e83ff8874b8df78588290dd134ea51672bb5c33f
sqwertyl <andrewchen118@gmail.com>
2023-03-20 15:28:12 -0700

    use svg for pfp, add click interaction to @

--- next.config.js
+++ next.config.js
@@ -5,18 +5,18 @@ const nextConfig = {
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-const repo = 'sqwertyl.me'
-let assetPrefix = ''
-let basePath = '/'
-
 if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}`
-  basePath = `/${repo}`
+  const repo = 'sqwertyl.me'
+  repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  let assetPrefix = `/${repo}`
+  let basePath = `/${repo}`
+  module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+  }
 }
 
 module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
-
+  assetPrefix: '',
+  basePath: '',
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,43 +14,61 @@ library.add(faGithub);
 const fira = Fira_Code({ subsets: ['latin'] })
 
 const App = () => {
-  const [bgPosition, setBgPosition] = useState({x:0, y:0});
+  const [bgPosition, setBgPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
+    // slide in effect
     const profilePicture = document.getElementById("profilePicture");
     setTimeout(() => {
       content.classList.add("opacity-100", "slideIn");
       profilePicture.classList.add("opacity-100", "slideIn");
     }, 100);
-  
+
+    // perspective shifting and background parallax (disabled on mobile)
     const profileImage = document.getElementById("profileImage");
-  
-    // Check if the device is mobile
     const isMobileDevice = window.matchMedia("(max-width: 767px)").matches;
-  
-    // Only apply perspective shift on non-mobile devices
     if (!isMobileDevice) {
       window.addEventListener("mousemove", (event) => {
         const x =
-          ((event.clientX - window.innerWidth / 2) / window.innerWidth) * 30;
-        const y = (event.clientY / window.innerHeight) * 30 - 20;
-  
+          ((event.clientX - window.innerWidth / 2) / window.innerWidth) * 40;
+        const y = (event.clientY / window.innerHeight) * 40 - 25;
+
         profileImage.style.transform = `perspective(1000px) rotateX(${-y}deg) rotateY(${x}deg)`;
-        setBgPosition({ x: x / 5, y: y / 5 });
+        setBgPosition({ x: x / 3, y: y / 3 });
       });
     }
+
+    // create on click event for the caption text
+    const captionText = document.getElementById("captionText");
+    const socialModal = document.getElementById("socialModal");
+    captionText.addEventListener("click", () => {
+      captionText.classList.toggle("caption-text");
+      captionText.classList.toggle("caption-text-clicked");
+      socialModal.classList.toggle("social-modal-clicked");
+      if (captionText.classList.contains("caption-text")) {
+        socialModal.style.transform.concat(" scale(1.1)");
+      }
+      
+    });
+
+    // socialModal.addEventListener("mouseenter", () => {
+    //   socialModal.style.transform = `scale(1.1)`;
+    // });
+
+
   }, []);
-  
+
+  // hopping name
   const name = "andrew chen";
   const nameElements = name.split("").map((letter, index) => {
     return (
       <span
         key={index}
-        className={letter===" " ? "space" : "hop-animation"}
+        className={letter === " " ? "space" : "hop-animation"}
         style={
-          letter===" "
+          letter === " "
             ? { ...fira.style }
-            : { animationDelay: `${index*0.1}s`, ...fira.style }
+            : { animationDelay: `${index * 0.1}s`, ...fira.style }
         }
       >
         {letter}
@@ -63,30 +81,30 @@ const App = () => {
     <div>
       <Head>
         <title>@sqwertyl</title>
-        <link rel="icon" type="image/png" href="favicon.png"/>
+        <link rel="icon" type="image/png" href="favicon.png" />
       </Head>
-      <div className={`min-h-screen flex items-center fade-in`} style={{backgroundImage:"url('bg.svg')", backgroundPosition:`${bgPosition.x}px ${bgPosition.y}px`}}>
+      <div className={`min-h-screen flex items-center fade-in`} style={{ backgroundImage: "url('bg.svg')", backgroundPosition: `${bgPosition.x}px ${bgPosition.y}px` }}>
         <div className="container mx-auto px-4 flex flex-col items-center">
-        <div className={`mb-10 opacity-0`} id="profilePicture">
-          <img id="profileImage" src="pfp.png" alt="Profile picture" className={`profile-picture rounded-full w-56 h-56`} draggable={false} />
-        </div>
-        <div className={`text-center opacity-0 content`} id="content">
-          <h1 className={`text-5xl font-bold mb-2`}>{nameElements}</h1>
-          <div className="relative group">
-            <div className="inline-block" id="captionContainer">
-              <p className={`text-2xl caption-text`} style={fira.style}>@sqwertyl</p>
-            </div>
-            <div className={`social-modal top-200 left-1/4 absolute w-1/2 h-14`}>
-              {/* Add your social icons here */}
-              <a href="https://github.com/sqwertyl" target="_blank" rel="noopener noreferrer" className="social-link">
-                <FontAwesomeIcon icon={faGithub} size="lg"/>
-              </a>
-              <a href="https://linkedin.com/in/andrewchen118" target="_blank" rel="noopener noreferrer" className="social-link">
-              <FontAwesomeIcon icon={faLinkedin} size="lg"/>
-              </a>
-              <span className={`absolute top-0 left-0 right-0 mx-auto w-full h-0 -mt-2 border-4 opacity-0`}></span>
-            </div>
+          <div className={`mb-10 opacity-0`} id="profilePicture">
+            <img id="profileImage" src="pfp.svg" alt="Profile picture" className={`profile-picture rounded-full w-56 h-56`} draggable={false} />
           </div>
+          <div className={`text-center opacity-0 content`} id="content">
+            <h1 className={`text-5xl font-bold mb-2`}>{nameElements}</h1>
+            <div className="relative group">
+              <div className="inline-block" id="captionContainer">
+                <p className={`text-2xl font-medium cursor-pointer caption-text`} style={fira.style} id="captionText">@sqwertyl</p>
+              </div>
+              <div className={`social-modal top-200 left-1/4 absolute w-1/2 h-14`} id='socialModal'>
+                {/* Add your social icons here */}
+                <a href="https://github.com/sqwertyl" target="_blank" rel="noopener noreferrer" className="social-link">
+                  <FontAwesomeIcon icon={faGithub} size="lg" />
+                </a>
+                <a href="https://linkedin.com/in/andrewchen118" target="_blank" rel="noopener noreferrer" className="social-link">
+                  <FontAwesomeIcon icon={faLinkedin} size="lg" />
+                </a>
+                <span className={`absolute top-0 left-0 right-0 mx-auto w-full h-0 -mt-2 border-4 opacity-0`}></span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
